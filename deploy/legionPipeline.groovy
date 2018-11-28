36514bb389eb5a5702a2ca8b2453a058b4b5463a
Alexander Semenets <am.semenets@gmail.com>
2018-11-28 10:27:03 +0300

    [#566] copy profile templates to image

--- deploy/legionPipeline.groovy
+++ deploy/legionPipeline.groovy
@@ -21,7 +21,7 @@ def createCluster() {
     file(credentialsId: "vault-${params.Profile}", variable: 'vault')]) {
         withAWS(credentials: 'kops') {
             wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
-                docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                     stage('Create cluster') {
                         sh """
                         cd /opt/legion/deploy/ansible && ansible-playbook create-cluster.yml \
@@ -63,7 +63,7 @@ def terminateCluster() {
     file(credentialsId: "vault-${params.Profile}", variable: 'vault')]) {
         withAWS(credentials: 'kops') {
             wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
-                docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                     stage('Create cluster') {
                         sh """
                         cd /opt/legion/deploy/ansible && ansible-playbook terminate-cluster.yml \
@@ -84,7 +84,7 @@ def deployLegion() {
         withAWS(credentials: 'kops') {
             wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
                 if (params.PublicRelease){
-                    docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                    docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                         stage('Deploy Legion') {
                             sh """
                             cd /opt/legion/deploy/ansible && ansible-playbook deploy-legion.yml \
@@ -97,7 +97,7 @@ def deployLegion() {
                         }
                     }
                 } else {
-                    docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                    docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                         stage('Deploy Legion') {
                             sh """
                             cd /opt/legion/deploy/ansible && ansible-playbook deploy-legion.yml \
@@ -280,7 +280,7 @@ def deployLegionEnclave() {
             withAWS(credentials: 'kops') {
                 wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
                     if (params.PublicRelease){
-                        docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                        docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                             stage('Deploy Legion Enclave') {
                                 sh """
                                 cd /opt/legion/deploy/ansible && ansible-playbook deploy-legion-enclave.yml \
@@ -294,7 +294,7 @@ def deployLegionEnclave() {
                             }
                         }
                     } else {
-                        docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                        docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                             stage('Deploy Legion Enclave') {
                                 sh """
                                 cd /opt/legion/deploy/ansible && ansible-playbook deploy-legion-enclave.yml \
@@ -318,7 +318,7 @@ def terminateLegionEnclave() {
             file(credentialsId: "vault-${params.Profile}", variable: 'vault')]) {
             withAWS(credentials: 'kops') {
                 wrap([$class: 'AnsiColorBuildWrapper', colorMapName: "xterm"]) {
-                    docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -u root") {
+                    docker.image("${params.DockerRepo}/k8s-ansible:${params.LegionVersion}").inside("-e HOME=/opt/deploy/legion -v ${WORKSPACE}/deploy/profiles:/opt/legion/deploy/profiles -u root") {
                         stage('Terminate Legion Enclave') {
                             sh """
                             cd /opt/legion/deploy/ansible && ansible-playbook terminate-legion-enclave.yml \
